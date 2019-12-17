2d34ee20ac994250070eb55d4e8af0fd9c1f5b38
Muraker <contactpod@gmail.com>
2019-12-17 19:15:34 +0100

    Page update Structure + icone 🐬

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 
-
 zIndex = 100;
-
 $('.pep').pep({
   useCSSTranslation: false,
-  constrainTo: 'window',
+  constrainTo: '.main',
   initiate: function(){
     zIndex ++;
     this.$el.css({ zIndex: zIndex });
   }
 });
 
+$( '.hello, .avatar' ).each(function( index ) {
+  $(this).css({
+    left : Math.random() * ($('.main').width() - $(this).width()),
+    top : Math.random() * ($('.main').height() - $(this).height())
+  }).addClass('up');
+});
+
+$(window).resize(function(){
+  $( '.hello, .avatar' ).each(function( index ) {
+    $(this).css({
+      left : Math.random() * ($('.main').width() - $(this).width()),
+      top : Math.random() * ($('.main').height() - $(this).height())
+    }).addClass('up');
+  });
+});
